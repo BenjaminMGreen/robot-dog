cf12b992100bb04faedb7bc19be92aae8a3b05c3
benjamin <bengreen123@hotmail.co.uk>
2017-06-23 22:00:00 +0100

    improved it

--- shin.py
+++ shin.py
@@ -11,24 +11,61 @@ from solid.utils import *
 
 SEGMENTS = 32
 
-
-foot_radius = 10
-foot_height = 50
+foot_radius = 25
+foot_height = foot_radius * 2 + 10
 leg_base_diameter = 15
-leg_upper_diameter = 25
-leg_length = 150
+leg_upper_diameter = 20
+leg_length = 200
+
 
 def foot():
-    f = sphere(foot_radius) + cylinder(leg_base_diameter, foot_height - foot_radius, 0)
+    f = sphere(foot_radius) + cylinder(leg_base_diameter, foot_height, 0)
     f = hull()(f)
 
     return f
 
+
+def shin():
+    s = cylinder(leg_upper_diameter, leg_length, leg_base_diameter)
+
+    return s
+
+
+def knee_joint():
+    joint_width = 40
+    joint_height = 20
+    bearing_outer = 21
+    # bearing_inner = 8
+    bearing_width = 7
+    # spammyheight = 20
+
+    # joint base
+    j = up(2.5)(
+        cube([joint_width, joint_width, 5], True))
+    # Left and right bearing tabs
+    j += right(joint_width / 2 - bearing_width / 2)(
+        up((joint_height + bearing_outer / 2 + 5) / 2)(
+            cube([bearing_width, joint_width, joint_height + bearing_outer / 2 + 5], True)))
+
+    j += left(joint_width / 2 - bearing_width / 2)(
+        up((joint_height + bearing_outer / 2 + 5) / 2)(
+            cube([bearing_width, joint_width, joint_height + bearing_outer / 2 + 5], True)))
+
+    # Cut bearing slots
+    j -= up(joint_height)(
+        rotate([0, 90, 0])(
+            cylinder(bearing_outer/2, joint_width + 20, center=True)))
+
+    return j
+
+
 def assembly():
     # Your code here!
-    a = foot()
+    # a = foot() + up(foot_height-foot_radius)(shin())
+    a = knee_joint()
 
     return a
+
 
 if __name__ == '__main__':
     a = assembly()
